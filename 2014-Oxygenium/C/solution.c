7baf5efe688668e6896cedfadeccae2aa6e82537
az5112 <az5112@yahoo.com>
2016-08-08 09:53:06 +0200

    Improve readability
    Swap argument order and less with greater. This makes the call to
    add_element easier to read (add element if the element is greater
    (or less)).

--- 2014-Oxygenium/C/solution.c
+++ 2014-Oxygenium/C/solution.c
@@ -20,7 +20,7 @@ int less_equal(int a, int b)
 
 int add_element( VP vp, VP* arr, int begin, int end, cmp_ft cmp )
 {
-	while( (end > begin) && cmp(arr[end-1].val, vp.val) ) {
+	while( (end > begin) && cmp(vp.val, arr[end-1].val) ) {
 		end--;
 	}
 	arr[end] = vp;
@@ -45,8 +45,8 @@ int solution( int K, int A[], int N )
 			vp.val = A[j];
 			vp.pos = j;
 
-			mini_end = add_element( vp, mini, mini_begin, mini_end, greater_equal );
-			maxi_end = add_element( vp, maxi, maxi_begin, maxi_end, less_equal );
+			mini_end = add_element( vp, mini, mini_begin, mini_end, less_equal );
+			maxi_end = add_element( vp, maxi, maxi_begin, maxi_end, greater_equal );
 			if( (maxi[maxi_begin].val - mini[mini_begin].val) <= K ) {
 				j++;
 			}
